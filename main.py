65177d74d4c64465dbc00a022f46b288d4a25bf7
Greg908903 <grisha.novgorodtsev@gmail.com>
2022-02-25 21:23:27 +0300

    Сделал начало, надо следующим действием написать колизии.

---
+++ main.py
@@ -0,0 +1,130 @@
+import pygame
+import pygame_gui
+import sys
+import os
+
+FPS = 50
+
+pygame.init()
+size = WIDTH, HEIGHT = 700, 1000
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+field = [[0] * 10 for i in range(10)]
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+def show_statistic():
+    pass
+
+def start_screen():
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((342, 300), (150, 75)),
+                                                text='Начать игру',
+                                                manager=manager)
+    # statistic_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((342, 400), (150, 75)),
+    #                                             text='Статистика',
+    #                                             manager=manager)
+
+    while True:
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == start_button:
+                    return
+            manager.process_events(event)
+
+        manager.update(time_delta)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(player_group, all_sprites)
+        self.image = pygame.transform.scale(load_image('bucket.png', -1), (50, 60))
+        self.rect = self.image.get_rect().move(100, 800)
+        self.move_left = 0
+        self.move_right = 0
+
+    def update(self, *args):
+        if args and args[0] == 'move':
+            if self.move_left:
+                self.rect.x -= 3
+            if self.move_right:
+                self.rect.x += 3
+            return
+        if args and args[0].type == pygame.KEYDOWN:
+            if args[0].key == pygame.K_LEFT:
+                self.move_left = 1
+            if args[0].key == pygame.K_RIGHT:
+                self.move_right = 1
+        if args and args[0].type == pygame.KEYUP:
+            if args[0].key == pygame.K_LEFT:
+                self.move_left = 0
+            if args[0].key == pygame.K_RIGHT:
+                self.move_right = 0
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.radius = 15
+        self.image = pygame.Surface((2 * self.radius, 2 * self.radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (self.radius, self.radius), self.radius)
+        self.rect = self.image.get_rect().move(100, 100)
+
+    def update(self, *args):
+        self.rect.y += 4
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    screen.blit(fon, (0, 0))
+
+
+start_screen()
+
+all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+Player()
+Ball()
+
+fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        all_sprites.update(event)
+    all_sprites.update('move')
+    draw(screen)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(FPS)
